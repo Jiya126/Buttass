cf6e1338031f91f956fbf14c21da671c9aff06f5
Jiya126 <90051748+Jiya126@users.noreply.github.com>
2022-12-14 13:54:17 +0530

    Update sku_read.py

--- sku_read.py
+++ sku_read.py
@@ -1,16 +1,19 @@
 import cv2
 import pytesseract as pyt
 
+# read text from image
 def ocr(img):
     text = pyt.image_to_string(img)
     return text
 
+# process image for feeding as input to read text
 def preprocess(img):
     cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.GaussianBlur(img,(5,5),0)
     cv2.threshold(img,0,255,cv2.THRESH_BINARY)
     cv2.medianBlur(img,5)
     return img
+
 
 cap = cv2.VideoCapture(0)
 while(1):
